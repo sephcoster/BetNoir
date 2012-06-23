e12c32e06cf204ffca5d68607dbfc183bb517c25
sephcoster <sailfast@gmail.com>
2012-06-22 23:45:37 -0400

    added root page

--- src/noir_test/views/welcome.clj
+++ src/noir_test/views/welcome.clj
@@ -1,6 +1,5 @@
 (ns noir-test.views.welcome
   (:require [noir-test.views.common :as common]
-            [noir.content.getting-started])
   (:use [noir.core :only [defpage]]
         [hiccup.core :only [html]]))
 
@@ -22,3 +21,9 @@
     [:p "Hope you like it."]
     [:div#clickableThing "Click this text to see or hide something cool!"]
     [:div#showThing [:img {:src "http://upload.wikimedia.org/wikipedia/en/e/e8/Noir_vol_1.jpg"} ]]))
+
+(defpage "/" []
+  (common/site-layout
+	[:h1 "BetNoir"]
+	[:h3 "Entendre, and Expression of Faith."]
+	[:p "Messing around with hiccup, Noir, and Clojure, and this is the place I've chosen to do it.  Thanks for stumbling upon this site, but there's unfortunately not a whole lot here for you, most likely."]))
